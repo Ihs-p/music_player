d4ce901b81ab9b681646b2242ebda32cf81d14a3
ihsan <ihsanp3553@gmail.com>
2025-06-30 22:48:01 +0530

    bug fixed

--- src/data/tracks.js
+++ src/data/tracks.js
@@ -2,38 +2,38 @@ export const TRACKS = [
     {
         title:'CHAL',
         artist:'Dabzee,Rishi Roy,Fathima Jahana',
-        cover:'./images/delkochi.png',
-        src:'./songs/CHAL - Dabzee.mp3'
+        cover:'/images/delkochi.png',
+        src:'/songs/CHAL - Dabzee.mp3'
     },
      {
         title:'Dilshad',
         artist:'Yawar Abdul,sifar ',
-        cover:'./images/dilshad.png',
-        src:'./songs/Dilshad.mp3'
+        cover:'/images/dilshad.png',
+        src:'/songs/Dilshad.mp3'
     },
       {
         title:'Haaniya',
         artist:' Hanan shah,Jubair Muhammad',
-        cover:'./images/haniya.png',
-        src:'./songs/Haaniya.mp3'
+        cover:'/images/haniya.png',
+        src:'/songs/Haaniya.mp3'
     },
       {
         title:'Jhol',
         artist:'Maanu,Anural khalid',
-        cover:'./images/jhol.png',
-        src:'./songs/Jhol.mp3'
+        cover:'/images/jhol.png',
+        src:'/songs/Jhol.mp3'
     },
       {
         title:'Kho-gaye',
         artist:'Taaruk Raina,',
-        cover:'./images/kho-gaye.png',
-        src:'./songs/Kho-Gaye.mp3'
+        cover:'/images/kho-gaye.png',
+        src:'/songs/Kho-Gaye.mp3'
     },
       {
         title:'Sajde',
         artist:'faheem abdulla',
-        cover:'./images/sajde.png',
-        src:'./songs/Sajde.mp3'
+        cover:'/images/sajde.png',
+        src:'/songs/Sajde.mp3'
     },
 ]
 
