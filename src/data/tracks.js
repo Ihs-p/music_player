e0501114d2de0a4b875ca1fc1c2a690355ae8421
ihsan <ihsanp3553@gmail.com>
2025-07-01 12:21:42 +0530

    bug fixing

--- src/data/tracks.js
+++ src/data/tracks.js
@@ -2,38 +2,38 @@ export const TRACKS = [
     {
         title:'CHAL',
         artist:'Dabzee,Rishi Roy,Fathima Jahana',
-        cover:'/images/delkochi.png',
-        src:'/songs/CHAL.mp3'
+        cover:'https://res.cloudinary.com/dqwcdoo11/image/upload/v1751352378/delkochi_dokzdo.png',
+        src:'https://res.cloudinary.com/dqwcdoo11/video/upload/v1751351593/CHAL_yclp5k.mp3'
     },
      {
         title:'Dilshad',
         artist:'Yawar Abdul,sifar ',
-        cover:'/images/dilshad.png',
-        src:'/songs/Dilshad.mp3'
+        cover:'https://res.cloudinary.com/dqwcdoo11/image/upload/v1751352387/dilshad_oyhytv.png',
+        src:'https://res.cloudinary.com/dqwcdoo11/video/upload/v1751351588/Dilshad_gqwjl3.mp3'
     },
       {
         title:'Haaniya',
         artist:' Hanan shah,Jubair Muhammad',
-        cover:'/images/haniya.png',
-        src:'/songs/Haaniya.mp3'
+        cover:'https://res.cloudinary.com/dqwcdoo11/image/upload/v1751352382/haniya_rbz2mn.png',
+        src:'https://res.cloudinary.com/dqwcdoo11/video/upload/v1751351591/Haaniya_enaprf.mp3'
     },
       {
         title:'Jhol',
         artist:'Maanu,Anural khalid',
-        cover:'/images/jhol.png',
-        src:'/songs/Jhol.mp3'
+        cover:'https://res.cloudinary.com/dqwcdoo11/image/upload/v1751352381/jhol_px4rzm.png',
+        src:'https://res.cloudinary.com/dqwcdoo11/video/upload/v1751351589/Jhol_jqy9li.mp3'
     },
       {
         title:'Kho-gaye',
         artist:'Taaruk Raina,',
-        cover:'/images/kho-gaye.png',
-        src:'/songs/Kho-Gaye.mp3'
+        cover:'https://res.cloudinary.com/dqwcdoo11/image/upload/v1751352384/kho-gaye_gntzbt.png',
+        src:'https://res.cloudinary.com/dqwcdoo11/video/upload/v1751351587/Kho-Gaye_efvnhg.mp3'
     },
       {
         title:'Sajde',
         artist:'faheem abdulla',
-        cover:'/images/sajde.png',
-        src:'/songs/Sajde.mp3'
+        cover:'https://res.cloudinary.com/dqwcdoo11/image/upload/v1751352377/sajde_vhwmdi.png',
+        src:'https://res.cloudinary.com/dqwcdoo11/video/upload/v1751351603/Sajde_rnkx6z.mp3'
     },
 ]
 
